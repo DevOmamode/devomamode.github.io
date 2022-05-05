0f5cd88fb17ea56ebaf7e34daf6ed15cc544756f
DevOmamode <josephisomamode@gmail.com>
2022-05-05 12:36:25 +0100

    The initial repository files...

---
+++ font-awesome/index.php
@@ -0,0 +1,19 @@
+<?php
+include ("../includes/includables.php");
+include ("../includes/header.php");
+?>
+<meta name=”robots” content=”noindex, nofollow”/> 
+<title>Page Does Not Exist - <?php echo P_NAME;?></title>
+</head>
+<body>
+<div id='body'>
+<?php
+include ("../includes/topheader.php");
+include ("../includes/simplecat.php");
+echo "<div style='padding:25%;' align='center'><a href='".BASE_URL."' style='color:white; font-size:1.2em; background-color:black; font-weight:bold; border-radius:5px; padding:2%;'>RETURN HOME <i class='fa fa-home'></i></a></div>";
+include ("../includes/simplecat.php");
+include ("../includes/footer.php");
+?>
+</div>
+</body>
+</html>
